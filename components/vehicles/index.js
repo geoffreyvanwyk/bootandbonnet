ade99056e58a079bb996c2abf8014351241f0c1b
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2013-06-28 02:51:48 +0200

    Renamed files of the vehicles component.

--- components/vehicles/index.js
+++ components/vehicles/index.js
@@ -1,34 +1,47 @@
 "use strict";
+
 /**
- * Import dependencies.
+ * Import external modules.
  */
 
 var express = require('express');
 var engine = require('ejs-locals');
+
+/**
+ * Import built-in modules.
+ */
+
 var path = require('path');
-var map = require('../../app').map;
-var profile = require('./routes/profile'); 
 
 /**
  * Configure application.
  */
 
 var app = module.exports = express();
+
 app.configure(function() {
 	app.engine('ejs', engine);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
-	app.use('/components/vehicles/assets', express.static('components/vehicles/assets'));
+	app.use('/assets', express.static(path.join(__dirname, 'assets')));
 });
+
 app.configure('development', function() {
 	app.use(express.errorHandler());
 });
 
 /**
- * Route requests.
+ * Import routes.
  */
 
-app.map = map;
+var registration = require('./routes/registration');
+
+/**
+ * Map urls and HTTP verbs to routes.
+ */
+
+app.map = require('../../app').map;
+
 app.map(app, {
 	'/vehicle': {
 		'/add': {
