69c9db21dbe1ad2a09fdbf05ac1fee4e5528e929
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2013-12-17 10:55:44 +0200

    Refactored the Login component.
    Factored out the isLogin function. Passed loginErrors directly to
    login.showForm, instead of via a session cookie. Session cookies are
    only necessary with redirects.

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,7 @@
 
 /**
  * @file routes/login.js
- * Component: users
- * Purpose: Contains routes that handle user login.
+ * @summary Component: Login. Contains routes that handle user login.
  */
 
 /* Import external modules. */
@@ -31,7 +30,7 @@ var handleErrors = function (err, user) {
 	console.log(err);
 	console.log('==================== END ERROR MESSAGE ======================');
 	if (err.user) {
-		request.session.loginError = {
+		loginErrors = {
 			emailAddress: err.user.emailAddress,
 			emailError: '',
 			emailAlertType: 'error',
@@ -42,12 +41,12 @@ var handleErrors = function (err, user) {
 	}
 	switch (err.message) {
 		case 'The email address has not been registered.':
-			request.session.loginError.emailError = err.message;
-			login.showForm(request, response);
+			loginErrors.emailError = err.message;
+			login.showForm(request, response, loginErrors);
 			break;
 		case 'The password is wrong.':
-			request.session.loginError.passwordError = err.message;
-			login.showForm(request, response);
+			loginErrors.passwordError = err.message;
+			login.showForm(request, response, loginErrors);
 			break;
 		case 'You are already logged-in':
 			request.session.specialError = err.message;
@@ -59,21 +58,46 @@ var handleErrors = function (err, user) {
 	}
 };
 
+/**
+ * @summary Returns true, if a user is logged-in; false, otherwise.
+ *
+ * @param {object} request An HTTP request object received from the express.get() method.
+ * @param {object} response An HTTP response object received from the express.get() method.
+ *
+ * @returns {boolean}
+ */
+var isLoggedIn = function (request, response) {
+	var displayError = function () {
+		handleErrors(new Error('You are already logged-in.'), request, response);
+		return false;
+	}
+	return !!request.session.user || displayError();
+}
+
 var login = module.exports = {
 	/**
-	 * @summary Responds to HTTP GET /login. Displays the login-form, unless the user is logged-in, in which case the
-	 * error-page is displayed.
+	 * @summary Responds to HTTP GET /login. Displays the login-form.
 	 *
-	 * @description If the user is logged-in, the function displays the error-page, because there is no reason for a
-	 * logged-in user to see the login-form. In the user interface, there is no way for a logged-in user to request the
-	 * login-form, but a logged-in user might attempt to request it directly via the browser's location bar.
+	 * @description Preconditions:
+	 * (1) The user must not be logged-in (function isLoggedIn).
 	 *
-	 * If a log-in attempt fails, in other words, if the user enters an email address that is not in the database, or
-	 * if the password is wrong, the login-form is displayed again, but this time with appropriate error messages. If
-	 * the loginError property of the request.session object exists, it means that a log-in attempt has failed. The
-	 * loginError property of the request.session object is set in the authenticate function.
+	 * There is no reason for a logged-in user to see the log-in form. In the user interface, there is no way for a
+	 * logged-in user to request the login-form, but a logged-in user might attempt to request it directly via the
+	 * browser's location bar.
 	 *
-	 * After a user has reset his password, the login form is shown to him, with a banner message across the top
+	 * Postconditions:
+	 * (1) The views/login-form.ejs is displayed.
+	 *
+	 * Error handling:
+	 * (1) If the user is logged-in, the function displays the error-page, because there is no reason for a
+	 * logged-in user to see the login-form (function isLoggedIn()).
+	 *
+	 * (2) If a log-in attempt fails, in other words, if the user enters an email address that is not in the database,
+	 * or if the password is wrong, the login-form is displayed again, but this time with appropriate error messages. If
+	 * the loginErrors parameter is not null or undefined, it means that a log-in attempt has failed. The
+	 * loginErrors argument is passed by the authenticate function.
+	 *
+	 * (3) After a user has reset his password, the login form is shown to him, with a banner message across the top
 	 * of the form, telling him that the password reset was successful, and that he may log-in again. If the
 	 * isPasswordReset property of the request.session object is true, the banner is displayed. The isPasswordReset
 	 * property of the request.session object is set in the resetPassword function of the password-reset module.
@@ -83,13 +107,10 @@ var login = module.exports = {
 	 *
 	 * @returns {undefined}
 	 */
-	showForm: function (request, response) {
-		var isLoggedIn = request.session.user;
-		if (isLoggedIn) {
-			handleErrors(new Error('You are already logged-in.'));
-		} else {
+	showForm: function (request, response, loginErrors) {
+		if (!isLoggedIn(request, response)) {
 			response.render('login-form', {
-				loginError: request.session.loginError || {
+				loginErrors: loginErrors || {
 					emailAddress: '',
 					emailError: '',
 					emailAlertType: '',
@@ -99,14 +120,6 @@ var login = module.exports = {
 				},
 				resetDisplay: request.session.isPasswordReset ? '' : 'none',
 				isLoggedIn: false
-			}, function (err, html) {
-				if (err) {
-					handleErrors(err);
-				} else {
-					request.session.loginError = null;
-					request.session.isPasswordReset = false;
-					response.send(html);
-				}
 			});
 		}
 	},
@@ -114,8 +127,16 @@ var login = module.exports = {
 	 * @summary Responds to HTTP POST /login. Authenticates the user, logs-in the user, then displays the home
 	 * page.
 	 *
-	 * @description The user is authenticated by taking the following steps:
+	 * @description Preconditions:
+	 * (1) The user is not logged-in (function isLoggedIn()).
 	 *
+	 * Postconditions:
+	 * (1) The authenticated user (and seller) is logged-in by setting the session cookies (request.session.user;
+	 * request.session.seller).
+	 * (2) The home page is displayed.
+	 *
+	 * Algorithm:
+	 * The user is authenticated by taking the following steps:
 	 * 1) Use the findUser() closure function to check whether there is any user in the users database
 	 * collection with the email address entered into the login form.
 	 *
@@ -125,13 +146,11 @@ var login = module.exports = {
 	 * 3) If the hashes are the same, use the findSeller() closure function to retrieve the seller object
 	 * associated with the user, if any.
 	 *
-	 * After authentication, the user is logged-in by adding a user property to the request.session object,
-	 * then assigning the user object to it. If the user has an associated seller object, the seller object is assigned
-	 * to the seller property of the request.session object.
+	 * Error handling:
+	 * (1) If the user is logged-in, the function displays the error-page, because there is no reason for a
+	 * logged-in user to see the login-form (function isLoggedIn()).
 	 *
-	 * Finally, the home page is displayed by calling the main.showHomePage() function.
-	 *
-	 * If the email address does not exist in the database, or if the password is wrong, the handleErrors() function
+	 * (2) If the email address does not exist in the database, or if the password is wrong, the handleErrors() function
 	 * is called to redisplay the login-form with the errors.
 	 *
 	 * @param {object} request An HTTP request object received from the express.post() method.
@@ -140,63 +159,69 @@ var login = module.exports = {
 	 * @returns {undefined}
 	 */
 	authenticate: function (request, response) {
-		/* User object consisting of the email address and password entered into the login-form. */
-		var frmUser = request.body.user;
+		if (isLoggedIn(request, response)) {
+			/* User object consisting of the email address and password entered into the login-form. */
+			var frmUser = request.body.user;
 
-		/* Email address entered into the login-form. */
-		var frmEmailAddress = frmUser.emailAddress.toLowerCase().trim();
+			/* Email address entered into the login-form. */
+			var frmEmailAddress = frmUser.emailAddress.toLowerCase().trim();
 
-		/* Password entered into the login-form. */
-		var frmPassword = frmUser.password;
+			/* Password entered into the login-form. */
+			var frmPassword = frmUser.password;
 
-		var findSeller = function (user, callback) {
-			Seller.findOne({user: user._id}, function (err, seller) {
+			var findSeller = function (user, callback) {
+				Seller.findOne({user: user._id}, function (err, seller) {
+					if (err) {
+						return callback(err);
+					}
+					return callback(null, user, seller);
+				});
+			};
+
+			var comparePassword = function (user, callback) {
+				bcrypt.compare(frmPassword, user.passwordHash, function (err, isMatch) {
+					if (err) {
+						return callback(err);
+					}
+					if (isMatch) {
+						findSeller(user, callback);
+					} else {
+						var err = new Error('The password is wrong.');
+						err.user = frmUser;
+						return callback(err);
+					}
+				});
+			};
+
+			var findUser = function (callback) {
+				User.findOne({emailAddress: frmEmailAddress}, function (err, user) {
+					if (err) {
+						return callback(err);
+					}
+					if (!user) {
+						var err = new Error('The email address has not been registered.');
+						err.user = frmUser;
+						return callback(err);
+					}
+					comparePassword(user, callback);
+				});
+			};
+
+			findUser(function (err, user, seller) {
 				if (err) {
-					return callback(err);
-				}
-				return callback(null, user, seller);
-			});
-		};
-
-		var comparePassword = function (user, callback) {
-			bcrypt.compare(frmPassword, user.passwordHash, function (err, isMatch) {
-				if (err) {
-					return callback(err);
-				}
-				if (isMatch) {
-					findSeller(user, callback);
+					handleErrors(err, request, response);
 				} else {
-					var err = new Error('The password is wrong.');
-					err.user = frmUser;
-					return callback(err);
+					request.session.user = {
+						_id: user._id,
+						emailAddress: user.emailAddress,
+						isEmailAddressVerified: user.isEmailAddressVerified
+					};
+					if (seller) { // The new user might not be a seller; might be an administrator.
+						request.session.seller = seller;
+					}
+					main.showHomePage(request, response);
 				}
 			});
-		};
-
-		var findUser = function (callback) {
-			User.findOne({emailAddress: frmEmailAddress}, function (err, user) {
-				if (err) {
-					return callback(err);
-				}
-				if (!user) {
-					var err = new Error('The email address has not been registered.');
-					err.user = frmUser;
-					return callback(err);
-				}
-				comparePassword(user, callback);
-			});
-		};
-
-		findUser(function (err, user, seller) {
-			if (err) {
-				handleErrors(err);
-			} else {
-				request.session.user = user;
-				if (seller) {
-					request.session.seller = seller;
-				}
-				main.showHomePage(request, response);
-			}
-		});
+		}
 	}
 };
