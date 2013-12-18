c86659b087bb5845b6cd12ed840bd5eccc219d7e
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2013-12-18 05:18:18 +0200

    Refactored models/makes.js

--- models/makes.js
+++ models/makes.js
@@ -1,27 +1,52 @@
-"use strict";
+/*jshint node: true*/
 
+'use strict';
+
+/**
+ * @file models/makes.js
+ * @summary Component: Vehicle Registration. Defines Mongoose model for make objects.
+ */
+
+/* Import external modules. */
 var mongoose = require('mongoose');
+
+/* Import models. */
 var Vehicle = require('./vehicles').Vehicle;
 
+/* Models for vehicle models. */
 var modelSchema = mongoose.Schema({
-	name: String,
-	shape: {type: String, default: 'general'},
-	photo: {type: String, default: 'unknown' }, // File path to image.
-	vehicles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle'}]
+	name: {
+		type: String
+	},
+	shape: {
+		type: String,
+		default: 'general'
+	},
+	photo: { // File path to image.
+		type: String,
+		default: 'unknown'
+	},
+	vehicles: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Vehicle'
+	}]
 });
 
 modelSchema.index({name: 1}, {unique: true});
 
+/* Make model. */
 var makeSchema = mongoose.Schema({
-	name: String,
-	emblem: {type: String, default: 'unknown'}, // File path to image.
+	name: {
+		type: String,
+		required: true
+	},
+	emblem: { // File path to image.
+		type: String,
+		default: 'unknown'
+	},
 	models: [modelSchema]
 });
 
 makeSchema.index({name: 1}, {unique: true});
 
-var Make = mongoose.model('Make', makeSchema);
-
-module.exports = {
-	Make: Make
-};
+module.exports = mongoose.model('Make', makeSchema);
